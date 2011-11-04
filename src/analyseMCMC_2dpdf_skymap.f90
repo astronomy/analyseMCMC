2747a2a322c9e989f43caa1ff6dc7bed06ad2e57
MvdS <git@vandersluys.nl>
2011-11-04 17:23:00 +0100

    Added licence/copyright info to source files

--- src/analyseMCMC_2dpdf_skymap.f90
+++ src/analyseMCMC_2dpdf_skymap.f90
@@ -1,5 +1,23 @@
 !> \file analyseMCMC_2dpdf_skymap.f90  Routines to plot a 2D PDF on a sky map
 
+! 
+! LICENCE:
+! 
+! Copyright 2007-2011 Marc van der Sluys
+!  
+! This file is part of the AnalyseMCMC package.
+!  
+! This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+! by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+! 
+! This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+! 
+! You should have received a copy of the GNU General Public License along with this code (LICENSE).  If not, see 
+! <http://www.gnu.org/licenses/>.
+! 
+
+
 
 !***********************************************************************************************************************************
 !> \brief  Plot a sky map
--- src/compPDFs_functions.f90
+++ src/compPDFs_functions.f90
@@ -1,6 +1,25 @@
 !> \file compPDFs_functions.f90  Plot functions for compPDFs  (1D/2D PDFs and waveforms)
-!!
-!! \todo put file reading in separate routines
+
+! 
+! LICENCE:
+! 
+! Copyright 2007-2011 Marc van der Sluys
+!  
+! This file is part of the AnalyseMCMC package.
+!  
+! This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+! by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+! 
+! This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+! 
+! You should have received a copy of the GNU General Public License along with this code (LICENSE).  If not, see 
+! <http://www.gnu.org/licenses/>.
+! 
+
+
+
+!> \todo put file reading in separate routines
 
 
 !***********************************************************************************************************************************
