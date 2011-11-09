3e3a9ee6ea8802c6f3bb2e3ae491140ec2f2ffd5
MvdS <git@vandersluys.nl>
2011-11-09 17:29:24 +0100

    * FileList: specify code_version_p(g/l)plot.f90 here * Updated headers for all source files

--- CMake/FileList.cmake
+++ CMake/FileList.cmake
@@ -1,4 +1,4 @@
-set(AnalyseMCMC_SRC_FILES
+set( AnalyseMCMC_SRC_FILES
   src/analyseMCMC_1dpdfs.f90
   src/analyseMCMC_2dpdf_routines.f90
   src/analyseMCMC_2dpdf_skymap.f90
@@ -12,12 +12,19 @@ set(AnalyseMCMC_SRC_FILES
   src/analyseMCMC_stats.f90
   src/analyseMCMC_tailored.f90
   src/analyseMCMC_textroutines.f90
-)
+  )
 
+# Source files specific to PGPlot or PLplot:
 if( PLplot_FOUND )
-  set(AnalyseMCMC_SRC_FILES
+  set( AnalyseMCMC_SRC_FILES
     ${AnalyseMCMC_SRC_FILES}
     src/PG2PLplot.f90
+    src/code_version_plplot.f90
+    )
+else( PLplot_FOUND )
+  set( AnalyseMCMC_SRC_FILES
+    ${AnalyseMCMC_SRC_FILES}
+    src/code_version_pgplot.f90
     )
 endif( PLplot_FOUND )
 
