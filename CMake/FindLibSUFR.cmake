2d2c2977a03c62528412cb50497cf6aeeceba5f4
MvdS <git@vandersluys.nl>
2011-11-13 13:54:16 +0100

    Updated CMake files to CMakeFiles rev.21

--- CMake/FindLibSUFR.cmake
+++ CMake/FindLibSUFR.cmake
@@ -1,5 +1,6 @@
-# Adapted from FindPGPLOT.cmake
-# MvdS, 27/12/2010
+## FindLibSUFR.cmake:
+## Check for the presence of the LibSUFR headers and libraries
+## MvdS, 27/12/2010
 
 
 # Check for the presence of the LibSUFR headers and libraries
