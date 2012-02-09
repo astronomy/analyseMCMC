8e6759e9db214bc78f4971177af20d1d90447d2d
MvdS <git@vandersluys.nl>
2012-02-09 14:04:25 +0100

    BUGFIX - minor issue introduced in rev.254

--- src/analyseMCMC_functions.f90
+++ src/analyseMCMC_functions.f90
@@ -483,8 +483,8 @@ subroutine read_mcmcfiles(exitcode)
   use SUFR_constants, only: stdErr,stdOut
   use SUFR_system, only: quit_program_error
   
-  use analysemcmc_settings, only: thin,maxChLen, maxMCMCpar,parNames
-  use general_data, only: allDat,post,prior,ntot,n,nchains,nchains0,infiles,maxIter
+  use analysemcmc_settings, only: thin,maxChLen, maxMCMCpar
+  use general_data, only: allDat,post,prior,ntot,n,nchains,nchains0,infiles,maxIter, parNames
   use mcmcrun_data, only: niter,Nburn0,detnames,detnr,parID,seed,snr,revID,ndet,flow,fhigh,t_before,nCorr,nTemps,Tmax,Tchain
   use mcmcrun_data, only: networkSNR,waveform,pnOrder,nMCMCpar,t_after,FTstart,deltaFT,samplerate,samplesize,FTsize,outputVersion
   use mcmcrun_data, only: nMCMCpar0,t0,GPStime
