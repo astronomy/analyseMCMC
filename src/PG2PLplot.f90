846e663f6e416d264812b590538cb2c5f2671530
Marc van der Sluys <git@vandersluys.nl>
2022-07-24 13:36:18 +0200

    Update copyright and licence.

--- src/PG2PLplot.f90
+++ src/PG2PLplot.f90
@@ -7,14 +7,14 @@
 !  
 ! This file is part of the PG2PLplot package.
 !  
-! This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
-! by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+! This is free software: you can redistribute it and/or modify it under the terms of the European Union
+! Public Licence 1.2 (EUPL 1.2).
 ! 
 ! This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the EU Public License for more details.
 ! 
-! You should have received a copy of the GNU General Public License along with this code (LICENCE).  If not, see 
-! <http://www.gnu.org/licenses/>.
+! You should have received a copy of the European Union Public License along with this code.  If not, see
+! <https://www.eupl.eu/1.2/en/>.
 ! 
 
 
